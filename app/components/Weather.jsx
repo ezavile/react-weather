1f2b57e529fdcfcb8cfb06f66d0c1b842ac12cb1
Edgar Zavala <ezavala@collectivemedia.mx>
2016-11-08 22:34:12 -0600

    first commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+
+var Weather = React.createClass({
+  getInitialState: function() {
+    return {
+      location: 'Miami',
+      temp: 80
+    }
+  },
+  handleSearch: function(location) {
+    this.setState({
+      location: location,
+      temp: 23
+    })
+  },
+  render: function () {
+    var {temp, location} = this.state;
+
+    return (
+      <div>
+        <h3>Weather Component</h3>
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage temp={temp} location={location}/>
+      </div>
+    )
+  }
+});
+
+module.exports = Weather;
