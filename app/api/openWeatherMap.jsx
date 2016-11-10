824fa5241b4928f4355250ad2642fb124b0aa7e1
Edgar Zavala <ezavala@collectivemedia.mx>
2016-11-10 00:09:53 -0600

    adding loading status

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -14,7 +14,7 @@ module.exports = {
                 return res.data.main.temp;
             }
         }, function(res) {
-            throw new Error(res.data.message);
+            throw new Error(res.response.data.message);
         });
     }
 }
