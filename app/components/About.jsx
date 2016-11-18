43f2cc05bd0db2cfd32cf752e0bf7962b9c9d9e9
ezavile <ezavile@gmail.com>
2016-11-17 23:46:48 -0600

    adding style to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,19 @@ var React = require('react');
 // Stateless functional component
 var About = (props) => {
   return (
-      <h3>About Component</h3> 
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build on React. I have built this following the course <a href="https://www.udemy.com/the-complete-react-web-app-developer-course" target="_blank">"The Complete React Web App Developer Course".</a></p> 
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react/">React</a> - This was the JavaScript library used.
+        </li>
+        <li>
+          <a href="https://openweathermap.org/">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   )
 }
 
