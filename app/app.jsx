18d7bebd58fca4be7ca3365a7f33d82cc3f340fb
ezavile <ezavile@gmail.com>
2016-11-19 01:32:36 -0600

    switch to SCSS

--- app/app.jsx
+++ app/app.jsx
@@ -10,7 +10,7 @@ var Examples = require('Examples');
 require('style!css!foundation-sites/dist/foundation.min.css');
 
 // load app.css
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 $(document).foundation();
 
